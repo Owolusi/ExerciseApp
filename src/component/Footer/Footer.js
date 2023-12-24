e6ff969a36d6232bbda32612f5c8cbb41089bec7
Owolusi Lucky <109273986+Owolusi@users.noreply.github.com>
2023-12-24 02:16:35 +0100

    resolving dev error6

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => (
     </Stack>
     <Typography variant="h5" 
     sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">
-      @Owolusi Lucky</Typography>
+      @Owolusi Lucky MightyTech</Typography>
   </Box>
 );
 
